a0afc5bb2af12ad63033941725aa6d78b4e5a31a
Michel Cojocaru <michel.cojo@gmail.com>
2025-03-24 13:28:42 +0200

    use service for calculations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { UserInputComponent } from './user-input/user-input.component';
 import { InvestmentInput } from './investment-input.model';
 import { InvestmentResult } from './investment-result.model';
 import { InvestmentResultsComponent } from './investment-results/investment-results.component';
+import { InvestmentService } from './investment.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,28 +14,7 @@ import { InvestmentResultsComponent } from './investment-results/investment-resu
 export class AppComponent {
   resultsData = signal<InvestmentResult[] | undefined>(undefined);
 
-  onCalculateInvestmentResults(data: InvestmentInput) {
-    const { initialInvestment, annualInvestment, expectedReturn, duration } =
-      data;
-    const annualData = [];
-    let investmentValue = initialInvestment;
-
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-
-    this.resultsData.set(annualData);
+  constructor(private investmentService: InvestmentService) {
+    this.resultsData.set(this.investmentService.resultsData());
   }
 }
